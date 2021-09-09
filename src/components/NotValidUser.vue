97d2059f30881ee43eedf5b53e703b1bcae99ed9
anand-jm <anand.prakash@jupiter.money>
2021-09-09 16:02:38 +0530

    ttesting for reading config

--- src/components/NotValidUser.vue
+++ src/components/NotValidUser.vue
@@ -7,23 +7,7 @@ Please contact to Data Science Team
 
 <script>
 export default {
+  name: 'NotValidUser'
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
